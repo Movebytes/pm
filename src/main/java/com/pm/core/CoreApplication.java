7b8b5e0efc3382f06ca01ad3c440698eaf586281
Ruslan Butyk <movebytes@gmail.com>
2018-10-07 01:10:17 +0300

    add initializer; remove useless code

--- src/main/java/com/pm/core/CoreApplication.java
+++ src/main/java/com/pm/core/CoreApplication.java
@@ -1,11 +1,9 @@
-package core;
+package com.pm.core;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class CoreApplication extends Application {
-    public CoreApplication() {
 
-    }
 }
